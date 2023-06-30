fb9883f4b2cff6f7de6252120194f9b6e72a6df1
cenunix <caden.hargrave@gmail.com>
2023-06-29 19:23:58 -0700

    Add: bluetooth set address using service

--- hosts/callisto/services.nix
+++ hosts/callisto/services.nix
@@ -1,31 +1,39 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, ...
 }:
 #Services for x13s battery-sound etc. functionality, relies on firmware
 {
-  systemd.services.pd-mapper = {
-    unitConfig = {
-      Requires = "qrtr-ns.service";
-      After = "qrtr-ns.service";
+  systemd.services = {
+    pd-mapper = {
+      unitConfig = {
+        Requires = "qrtr-ns.service";
+        After = "qrtr-ns.service";
+      };
+      serviceConfig = {
+        Restart = "always";
+        ExecStart = "${pkgs.pd-mapper}/bin/pd-mapper";
+      };
+      wantedBy = [
+        "multi-user.target"
+      ];
     };
-    serviceConfig = {
-      Restart = "always";
-      ExecStart = "${pkgs.pd-mapper}/bin/pd-mapper";
+    qrtr-ns = {
+      serviceConfig = {
+        ExecStart = "${pkgs.qrtr}/bin/qrtr-ns -f 1";
+        Restart = "always";
+      };
+      wantedBy = [ "multi-user.target" ];
     };
-    wantedBy = [
-      "multi-user.target"
-    ];
-  };
-  systemd.services.qrtr-ns = {
-    serviceConfig = {
-      ExecStart = "${pkgs.qrtr}/bin/qrtr-ns -f 1";
-      Restart = "always";
+    bt-address = {
+      serviceConfig = {
+        ExecStart = "${pkgs.bluez5-experimental}/bin/btmgmt public-addr F4:A8:0D:30:A3:47";
+        Restart = "always";
+      };
+      wantedBy = [ "multi-user.target" ];
     };
-    wantedBy = ["multi-user.target"];
   };
 }
