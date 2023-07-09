55308fba3f50c770587ebb576638b1f040663895
cenunix <caden.hargrave@gmail.com>
2023-07-09 12:10:01 -0700

    Update: nvim config

--- pkgs/astronvim/user/plugins/user.lua
+++ pkgs/astronvim/user/plugins/user.lua
@@ -13,6 +13,17 @@ return {
   -- {
   --   "nvim-treesitter/nvim-treesitter", enabled = false
   -- },
+  { "lervag/vimtex",                    lazy = false },
+  "toppair/peek.nvim",
+  build = "deno task --quiet build:fast",
+  config = function()
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+  cmd = {
+    "PeekOpen",
+    "PeekClose",
+  },
   { import = "astrocommunity.pack.yaml" },
   {
     "akinsho/flutter-tools.nvim",
