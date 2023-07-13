c8ba26997634108fbc2c6f396da46f58f267c73b
cenunix <caden.hargrave@gmail.com>
2023-07-13 14:21:03 -0700

    Add: schizofox nvidia prefs

--- modules/common/system/module/programs.nix
+++ modules/common/system/module/programs.nix
@@ -3,27 +3,6 @@ with lib; {
   # this module provides overrides for certain defaults and lets you set
   # default programs for referencing in other config files.
   options.modules = {
-    services = {
-      override = {
-        nextcloud = mkEnableOption "Override Nextcloud service";
-        mailserver = mkEnableOption "Override nixos-mailserver service";
-        mkm = mkEnableOption "Override mkm-ticketing service";
-        vaultwarden = mkEnableOption "Override VaultWarden service";
-        gitea = mkEnableOption "Override Gitea service";
-        grafana = mkEnableOption "Override Grafana and Prometheus services";
-        irc = mkEnableOption "Override Quassel IRC service";
-        jellyfin = mkEnableOption "Override Jellyfin media service";
-        matrix = mkEnableOption "Override Matrix-synapse service";
-        wireguard = mkEnableOption "Override Wireguard service";
-        database = {
-          mysql = mkEnableOption "Override MySQL database service";
-          mongodb = mkEnableOption "Override MongoDB service";
-          redis = mkEnableOption "Override Redis service";
-          postgresql = mkEnableOption "Override Postgresql service";
-        };
-      };
-    };
-
     programs = {
       # "override" is a simple option that sets the programs' state to the oppossite of their default
       override = {
