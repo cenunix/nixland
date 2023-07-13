7ff6f12c56f27036b2676b54dbe6c45b0f4b8ef6
cenunix <caden.hargrave@gmail.com>
2023-07-13 15:05:23 -0700

    Update: anyrun config

--- home/cenunix/graphical/launchers/anyrun/default.nix
+++ home/cenunix/graphical/launchers/anyrun/default.nix
@@ -6,59 +6,30 @@
   pkgs,
   ...
 }: let
-  inherit (inputs.anyrun.packages.${pkgs.system}) anyrun;
+  anyrunPkgs = inputs.anyrun.packages.${pkgs.system};
 in {
-  home.packages = [anyrun];
+  imports = [
+    inputs.anyrun.homeManagerModules.default
+  ];
+  programs.anyrun = {
+    enable = true;
 
-  xdg.configFile = {
-    "anyrun/config.ron".text = ''
-      Config(
-        // `width` and `vertical_offset` use an enum for the value it can be either:
-        // Absolute(n): The absolute value in pixels
-        // Fraction(n): A fraction of the width or height of the full screen (depends on exclusive zones and the settings related to them) window respectively
+    config = {
+      plugins = with anyrunPkgs; [
+        applications
+        rink
+        symbols
+        dictionary
+        kidex
+        stdin
+        shell
+        translate
+      ];
 
-        // How wide the input box and results are.
-        width: Fraction(0.3),
-
-        // Where Anyrun is located on the screen: Top, Center
-        position: Top,
-
-        show_results_immediately: false,
-
-        // How much the runner is shifted vertically
-        vertical_offset: Absolute(15),
-
-        // Hide match and plugin info icons
-        hide_icons: false,
-
-        // ignore exclusive zones, f.e. Waybar
-        ignore_exclusive_zones: false,
-
-        // Layer shell layer: Background, Bottom, Top, Overlay
-        layer: Overlay,
-
-        // Hide the plugin info panel
-        hide_plugin_info: true,
-
-        // Close window when a click outside the main box is received
-        close_on_click: true,
-
-        // List of plugins to be loaded by default, can be specified with a relative path to be loaded from the
-        // `<anyrun config dir>/plugins` directory or with an absolute path to just load the file the path points to.
-        plugins: [
-          "${anyrun}/lib/libapplications.so",
-          "${anyrun}/lib/librandr.so",
-          "${anyrun}/lib/librink.so",
-          "${anyrun}/lib/libshell.so",
-          "${anyrun}/lib/libsymbols.so",
-          "${anyrun}/lib/libtranslate.so",
-          "${anyrun}/lib/libkidex.so",
-          "${anyrun}/lib/libshell.so",
-        ],
-      )
-    '';
-
-    "anyrun/style.css".text = ''
+      hidePluginInfo = true;
+      closeOnClick = true;
+    };
+    extraCss = ''
       * {
         transition: 200ms ease-out;
         font-family: sansSerif;
@@ -70,6 +41,7 @@ in {
       #entry,
       #plugin,
       #main {
+        color: #cad3f5;
         background: transparent;
       }
 
