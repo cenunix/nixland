3df8901d33fc5f055286f4e48cc5b1cfd593b809
cenunix <caden.hargrave@gmail.com>
2023-07-17 03:05:04 -0700

    add: nixvim-lsp

---
+++ home/cenunix/editors/nixvim/config/plugins/lsp.nix
@@ -0,0 +1,41 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  config = {
+    plugins = {
+      lsp = {
+        enable = true;
+
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            # Navigate in diagnostics
+            "<leader>k" = "goto_prev";
+            "<leader>j" = "goto_next";
+          };
+
+          lspBuf = {
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            "<F2>" = "rename";
+          };
+        };
+
+        servers = {
+          bashls.enable = true;
+          clangd.enable = true;
+          nil_ls.enable = true;
+          lua-ls.enable = true;
+        };
+      };
+    };
+  };
+}
