63b285d32c8d3314a903da1044b1f0ab63a083fd
cenunix <caden.hargrave@gmail.com>
2023-07-17 03:11:05 -0700

    add: telescope, treesitter

---
+++ home/cenunix/editors/nixvim/config/plugins/treesitter.nix
@@ -0,0 +1,40 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  config = {
+    plugins = {
+      treesitter = {
+        enable = true;
+
+        nixvimInjections = true;
+
+        folding = true;
+        indent = true;
+
+        grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+          bash
+          c
+          html
+          javascript
+          latex
+          lua
+          nix
+          norg
+          python
+          rust
+          vimdoc
+        ];
+      };
+
+      treesitter-refactor = {
+        enable = true;
+        highlightDefinitions.enable = true;
+      };
+    };
+  };
+}
