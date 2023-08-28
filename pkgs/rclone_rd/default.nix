d8f96a2e5195b59e6923c44b847c37ed3380a36d
cenunix <user555596@protonmail.com>
2023-08-27 22:48:40 -0700

    update mediaserver.nix

--- pkgs/rclone_rd/default.nix
+++ pkgs/rclone_rd/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchUrl
+, fetchurl
 , systemd
 ,
 }:
@@ -8,9 +8,9 @@ stdenv.mkDerivation {
   pname = "rclone_rd";
   version = "1.0";
 
-  src = fetchUrl {
-    url = "https://github.com/itsToggle/rclone_RD/releases/download/v1.58.1-rd.2.2/rclone-linux";
-    sha256 = "sha256-KF0gCBRw3BDJdK1s+dYhHkokVTHwRFO58ho0IwHPehc=";
+  src = fetchurl {
+    url = "https://github.com/itsToggle/rclone_RD/releases/download/v1.58.1-rd.2.2/z-rclone-as-beta-linux";
+    sha256 = "sha256-f+6H+F7p4jWNqf3qb36HXOtJZ5ixLJFA/ZqAH6iJkyU=";
   };
   phases = [ "installPhase" "patchPhase" ];
   installPhase = ''
