03b7f7d5ea3382936b5988a27ca8cdca68833fd2
cenunix <user555596@protonmail.com>
2023-08-27 23:57:27 -0700

    update rclone_rd pkg

--- pkgs/rclone_rd/default.nix
+++ pkgs/rclone_rd/default.nix
@@ -1,25 +1,71 @@
 { lib
 , stdenv
-, fetchurl
-, fuse3
-, systemd
-,
+, buildGoModule
+, fetchFromGitHub
+, buildPackages
+, installShellFiles
+, makeWrapper
+, enableCmount ? true
+, fuse
+, macfuse-stubs
+, librclone
 }:
-stdenv.mkDerivation {
-  pname = "rclone_rd";
-  version = "v1.58.1-rd.2.2";
 
-  src = fetchurl {
-    url = "https://github.com/itsToggle/rclone_RD/releases/download/v1.58.1-rd.2.2/z-rclone-as-beta-linux";
-    sha256 = "sha256-f+6H+F7p4jWNqf3qb36HXOtJZ5ixLJFA/ZqAH6iJkyU=";
+buildGoModule rec {
+  pname = "rclone_RD";
+  version = "1.63.1";
+
+  src = fetchFromGitHub {
+    owner = itsToggle;
+    repo = pname;
+    rev = "1a8b0dfc095262013c7285ea7ddf0a875143f582";
+    hash = "sha256-H//Y7BFBr3VXAoKZZgjSgU4aA+Af7tvFozhpoj14ba0=";
   };
-  propagatedBuildInputs = [
-    fuse3
-  ];
-  phases = [ "installPhase" "patchPhase" ];
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src $out/bin/rclone-linux
-    chmod +x $out/bin/rclone-linux
-  '';
+
+  vendorHash = "sha256-AXgyyI6ZbTepC/TGkHQvHiwpQOjzwG5ung71nKE5d1Y=";
+
+  subPackages = [ "." ];
+
+  outputs = [ "out" "man" ];
+
+  buildInputs = lib.optional enableCmount (if stdenv.isDarwin then macfuse-stubs else fuse);
+  nativeBuildInputs = [ installShellFiles makeWrapper ];
+
+  tags = lib.optionals enableCmount [ "cmount" ];
+
+  ldflags = [ "-s" "-w" "-X github.com/rclone/rclone/fs.Version=${version}" ];
+
+  postInstall =
+    let
+      rcloneBin =
+        if stdenv.buildPlatform.canExecute stdenv.hostPlatform
+        then "$out"
+        else lib.getBin buildPackages.rclone;
+    in
+    ''
+      installManPage rclone.1
+      for shell in bash zsh fish; do
+        ${rcloneBin}/bin/rclone genautocomplete $shell rclone.$shell
+        installShellCompletion rclone.$shell
+      done
+    '' + lib.optionalString (enableCmount && !stdenv.isDarwin)
+      # use --suffix here to ensure we don't shadow /run/wrappers/bin/fusermount,
+      # as the setuid wrapper is required as non-root on NixOS.
+      ''
+        wrapProgram $out/bin/rclone \
+          --suffix PATH : "${lib.makeBinPath [ fuse ] }" \
+          --prefix LD_LIBRARY_PATH : "${fuse}/lib"
+      '';
+
+  passthru.tests = {
+    inherit librclone;
+  };
+
+  meta = with lib; {
+    description = "Command line program to sync files and directories to and from major cloud storage";
+    homepage = "https://rclone.org";
+    changelog = "https://github.com/rclone/rclone/blob/v${version}/docs/content/changelog.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [ marsam SuperSandro2000 ];
+  };
 }
