ff4886cdb71c36838ee1ec2f6b64aa23181a63b3
cenunix <user555596@protonmail.com>
2023-08-27 23:51:14 -0700

    update rclone_rd pkg

--- pkgs/rclone_rd/default.nix
+++ pkgs/rclone_rd/default.nix
@@ -13,9 +13,10 @@ stdenv.mkDerivation {
     url = "https://github.com/itsToggle/rclone_RD/releases/download/v1.58.1-rd.2.2/z-rclone-as-beta-linux";
     sha256 = "sha256-f+6H+F7p4jWNqf3qb36HXOtJZ5ixLJFA/ZqAH6iJkyU=";
   };
-  buildInputs = [
+  propagatedBuildInputs = [
     fuse3
   ];
+  phases = [ "installPhase" "patchPhase" ];
   installPhase = ''
     mkdir -p $out/bin
     cp $src $out/bin/rclone-linux
