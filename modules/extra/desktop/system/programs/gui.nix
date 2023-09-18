8d44eaa40cf0a27d91d67a814c38b997050eecb9
cenunix <user555596@protonmail.com>
2023-09-18 01:36:00 -0700

    add gnome

--- modules/extra/desktop/system/programs/gui.nix
+++ modules/extra/desktop/system/programs/gui.nix
@@ -13,18 +13,22 @@ in
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+  hardware = mkIf (env.desktop == "Gnome") {
+    pulseaudio.enable = false;
+  };
   environment = {
-    systemPackages = with pkgs; [
-      # packages necessery for thunar thumbnails
-      xfce.tumbler
-      libgsf # odf files
-      ffmpegthumbnailer
-      ark # GUI archiver for thunar archive plugin
-      sshfs # FUSE-based filesystem that allows remote filesystems to be mounted over SSH
-      samba
-      fuse
-      gnome.nautilus
-    ];
+    systemPackages = with pkgs;
+      [
+        # packages necessery for thunar thumbnails
+        xfce.tumbler
+        libgsf # odf files
+        ffmpegthumbnailer
+        ark # GUI archiver for thunar archive plugin
+        sshfs # FUSE-based filesystem that allows remote filesystems to be mounted over SSH
+        samba
+        fuse
+        gnome.nautilus
+      ];
   };
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.samba.enable = true;
