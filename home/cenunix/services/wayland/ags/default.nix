bd71bbd603db16491dfdd758c14cdf3deee30747
cenunix <user555596@protonmail.com>
2023-09-29 15:57:03 -0700

    updates

--- home/cenunix/services/wayland/ags/default.nix
+++ home/cenunix/services/wayland/ags/default.nix
@@ -11,28 +11,28 @@ with lib; let
   acceptedTypes = [ "desktop" "laptop" "armlaptop" ];
 in
 {
-  # config = mkIf (builtins.elem device.type acceptedTypes) {
-  #   xdg.configFile = {
-  #     "ags".source = config.lib.file.mkOutOfStoreSymlink "/home/cenunix/Personal/nicks/home/cenunix/services/wayland/ags/config";
-  #   };
-  #   home = {
-  #     # file.".config/ags".source = ./config;
+  config = mkIf (builtins.elem device.type acceptedTypes) {
+    xdg.configFile = {
+      "ags".source = config.lib.file.mkOutOfStoreSymlink "/home/cenunix/Personal/nicks/home/cenunix/services/wayland/ags/config";
+    };
+    home = {
+      # file.".config/ags".source = ./config;
 
-  #     packages = with pkgs; [
-  #       inputs.ags.packages.${pkgs.system}.default
-  #       sassc
-  #       slurp
-  #       swww
-  #       hyprpicker
-  #       watershot
-  #       imagemagick
-  #       wl-gammactl
-  #       socat
-  #       upower
-  #       gnome.gnome-bluetooth
-  #       glib
-  #       pavucontrol
-  #     ];
-  #   };
-  # };
+      packages = with pkgs; [
+        inputs.ags.packages.${pkgs.system}.default
+        sassc
+        slurp
+        swww
+        hyprpicker
+        watershot
+        imagemagick
+        wl-gammactl
+        socat
+        upower
+        gnome.gnome-bluetooth
+        glib
+        pavucontrol
+      ];
+    };
+  };
 }
