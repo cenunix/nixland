a5c98f75da0380b0d1df1f811c3eb83d872b8d93
cenunix <user555596@protonmail.com>
2024-02-28 19:40:30 -0800

    cleanup hosts

--- hosts/callisto/configuration.nix
+++ hosts/callisto/configuration.nix
@@ -24,6 +24,7 @@
     ./hardware-configuration.nix
     ./system.nix
     ./services.nix
+    ../shared
   ];
   services.upower.enable = true;
   # nix.buildMachines = [
@@ -49,13 +50,6 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-      outputs.overlays.stable-packages
-      outputs.overlays.pinned-packages
-
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
@@ -73,13 +67,6 @@
     ];
     # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
-      allowUnsupportedSystem = true;
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "electron-25.9.0"
-        "freeimage-unstable-2021-11-01"
-      ];
     };
   };
 
---
+++ hosts/shared/default.nix
@@ -0,0 +1,36 @@
+# Shared config for hosts
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnsupportedSystem = true;
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-25.9.0"
+        "freeimage-unstable-2021-11-01"
+      ];
+    };
+  };
+}
