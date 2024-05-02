d3332b69c0facaad435e7ebfb485e8f648c19007
cenunix <user55596@protonmail.com>
2024-05-01 21:48:45 -0700

    update flake.lock

--- modules/common/core/services/ollama.nix
+++ modules/common/core/services/ollama.nix
@@ -1,8 +1,9 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+{
   # services.ollama.enable = true;
   # services.ollama.acceleration = "rocm";
-  environment.systemPackages = with pkgs; [
-    (ollama.override { acceleration = "cuda"; })
-    lmstudio
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   (ollama.override { acceleration = "cuda"; })
+  #   lmstudio
+  # ];
 }
