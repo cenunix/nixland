2fdfe1e40e492abe368bd5efbadcb5e5411e0fe3
cenunix <user55596@protonmail.com>
2024-05-10 17:17:40 -0700

    colorscheme update

---
+++ home/cenunix/services/wayland/ags/bak.nix
@@ -0,0 +1,71 @@
+{ inputs, pkgs, config, ... }: {
+  imports = [
+    inputs.ags.homeManagerModules.default
+    inputs.astal.homeManagerModules.default
+  ];
+
+  home.packages = with pkgs; [
+    bun
+    dart-sass
+    fd
+    brightnessctl
+    (swww.overrideAttrs (o: rec {
+      pname = "swww";
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        owner = "LFGae";
+        repo = pname;
+        rev = "432b7135af0bb34e4b0e55edf76ff516937bc7b0";
+        hash = "sha256-pX6fJrlNXe1W5ZO9nC9ikN1zoAcHDqErLS7EnglD33o=";
+      };
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = src + "/Cargo.lock";
+        allowBuiltinFetchGit = true;
+      };
+    }))
+    (mpvpaper.overrideAttrs (o: rec {
+      pname = "mpvpaper";
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        owner = "GhostNaN";
+        repo = pname;
+        rev = "f65700a3ecc9ecd8ca501e18a807ee18845f9441";
+        hash = "sha256-h+YJ4YGVGGgInVgm3NbXQIbrxkMOD/HtBnCzkTcRXH8=";
+      };
+    }))
+
+    inputs.matugen.packages.${system}.default
+    slurp
+    fzf
+    wf-recorder
+    wl-clipboard
+    wayshot
+    swappy
+    hyprpicker
+    pavucontrol
+    networkmanager
+    rofi-bluetooth
+    gtk3
+  ];
+
+  programs.astal = {
+    enable = true;
+    extraPackages = with pkgs; [ libadwaita ];
+  };
+  xdg.configFile."ags".source = config.lib.file.mkOutOfStoreSymlink
+    "/home/cenunix/NixLand/home/cenunix/services/wayland/ags/config";
+
+  # home.file = {
+  #   ".config/ags" = {
+  #     source = ./config;
+  #     recursive = true;
+  #   };
+  # };
+  programs.ags = {
+    enable = true;
+    # configDir = ./config;
+    # extraPackages = with pkgs; [
+    #   accountsservice
+    # ];
+  };
+}
--- home/cenunix/services/wayland/ags/default.nix
+++ home/cenunix/services/wayland/ags/default.nix
@@ -1,72 +1,20 @@
-{ inputs, pkgs, config, ... }: {
-  imports = [
-    inputs.ags.homeManagerModules.default
-    inputs.astal.homeManagerModules.default
-  ];
+{ inputs, lib, config, pkgs, ... }: {
+  imports = [ inputs.ags.homeManagerModules.default ];
 
-  home.packages = with pkgs; [
-    bun
-    dart-sass
-    fd
-    brightnessctl
-    # (swww.overrideAttrs (o: rec {
-    #   pname = "swww";
-    #   version = "master";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "LFGae";
-    #     repo = pname;
-    #     rev = "432b7135af0bb34e4b0e55edf76ff516937bc7b0";
-    #     hash = "sha256-pX6fJrlNXe1W5ZO9nC9ikN1zoAcHDqErLS7EnglD33o=";
-    #   };
-    #   cargoDeps = pkgs.rustPlatform.importCargoLock {
-    #     lockFile = src + "/Cargo.lock";
-    #     allowBuiltinFetchGit = true;
-    #   };
-    # }))
-    swww
-    # (mpvpaper.overrideAttrs (o: rec {
-    #   pname = "mpvpaper";
-    #   version = "master";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "GhostNaN";
-    #     repo = pname;
-    #     rev = "f65700a3ecc9ecd8ca501e18a807ee18845f9441";
-    #     hash = "sha256-h+YJ4YGVGGgInVgm3NbXQIbrxkMOD/HtBnCzkTcRXH8=";
-    #   };
-    #))
+  config = lib.mkIf config.ags.enable {
+    home.packages = [ pkgs.sassc pkgs.cava ];
 
-    inputs.matugen.packages.${system}.default
-    slurp
-    fzf
-    wf-recorder
-    wl-clipboard
-    wayshot
-    swappy
-    hyprpicker
-    pavucontrol
-    networkmanager
-    rofi-bluetooth
-    gtk3
-  ];
+    programs.ags = {
+      enable = true;
 
-  programs.astal = {
-    enable = true;
-    extraPackages = with pkgs; [ libadwaita ];
-  };
-  xdg.configFile."ags".source = config.lib.file.mkOutOfStoreSymlink
-    "/home/cenunix/NixLand/home/cenunix/services/wayland/ags/config";
+      configDir = ./config;
 
-  # home.file = {
-  #   ".config/ags" = {
-  #     source = ./config;
-  #     recursive = true;
-  #   };
-  # };
-  programs.ags = {
-    enable = true;
-    # configDir = ./config;
-    # extraPackages = with pkgs; [
-    #   accountsservice
-    # ];
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+        libdbusmenu-gtk3
+      ];
+    };
   };
 }
