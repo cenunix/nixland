c3ee3f3b4939c56db64c8e2e21f69505b12875ec
cenunix <user55596@protonmail.com>
2024-05-10 17:20:39 -0700

    colorscheme update

--- home/cenunix/services/wayland/ags/default.nix
+++ home/cenunix/services/wayland/ags/default.nix
@@ -1,20 +1,16 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [ inputs.ags.homeManagerModules.default ];
 
-  config = lib.mkIf config.ags.enable {
-    home.packages = [ pkgs.sassc pkgs.cava ];
+  programs.ags = {
+    enable = true;
 
-    programs.ags = {
-      enable = true;
+    configDir = ./config;
 
-      configDir = ./config;
-
-      extraPackages = with pkgs; [
-        gtksourceview
-        webkitgtk
-        accountsservice
-        libdbusmenu-gtk3
-      ];
-    };
+    extraPackages = with pkgs; [
+      gtksourceview
+      webkitgtk
+      accountsservice
+      libdbusmenu-gtk3
+    ];
   };
 }
